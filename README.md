0a14bb109257b56c51ba523b619bb716031c3c93
kairo741 <kairo.amorim2001@gmail.com>
2021-02-27 01:44:57 -0300

    Estrutura montada e readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# geradorJson
+
+# Gerador Json 
+
+![test](https://img.icons8.com/ultraviolet/2x/json.png)By Kairo ![test](https://img.icons8.com/ultraviolet/2x/json.png)
+
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+O programa faz:
+- Gera vários JSONs em arquivos .txt
+- Escolher qual ID vai começar
+- Escolher quantos vai gerar
+- Escolher o nome do .txt
+- ✨Magic ✨
+
+
+## Plugins
+
+Os seguintes plugins são utilizados:
+
+| Plugin | README |
+| ------ | ------ |
+| Colorama | [plugins/github/README.rst][RDcl] |
+| GitHub | [plugins/github/README.md][PlGh] |
+**Free Software, Hell Yeah!**
+
+[//]: # (These are reference links used in the body of this note and get stripped out when the markdown processor does its job. There is no need to format nicely because it shouldn't be seen. Thanks SO - http://stackoverflow.com/questions/4823468/store-comments-in-markdown-syntax)
+
+   [PlGh]: <https://github.com/joemccann/dillinger/tree/master/plugins/github/README.md>
+
+ [RDcl]: <https://github.com/tartley/colorama/blob/master/README.rst>
--- geradorJson.py
+++ geradorJson.py
@@ -1,10 +1,33 @@
+from pip._vendor.colorama import Fore, Back
 
-for i in range(30):
-    arquivo = open("testee.txt", 'a+')
+
+# Frescura para pintar letra
+print(Fore.CYAN + "Bem vindo oa gerador de Json!\n", Fore.MAGENTA + "by " + Back.WHITE + "Kairo rs", Fore.RESET, Back.RESET)
+
+
+# QUantidade de Json que vai gerar
+quantidadeLote = int(input("Quantidade de Json: "))
+
+# De qual ID vai começar, exemplo: No banco está no ID 270, digite 271 nesse campo
+idPlus = int(input("Começa de qual id? "))
+
+# Nome do arquivo sem o .txt, exemplo: jsons
+# Vai sair jsons.txt
+nome_arquivo = input('Nome do arquivo a ser editado (sem o .txt): ') + ".txt"
+
+
+print("Processando...")
+
+# Começa a escrever os JSON no arquivo txt
+for i in range(quantidadeLote):
+    # Abre o arquivo
+    arquivo = open(nome_arquivo, 'a+')
+
+    # Lê o arquivo
     texto = arquivo.readlines()
 
 
-
+    # Pega o que leu do arquivo e acrescenta um novo JSON
     texto.append('''{
     "categorias": [
         {
@@ -20,7 +43,7 @@ for i in range(30):
     "codigo": "10",
     "codigoBarras": "0326526",
     "descricao": "The Earth Dragon Balls are about 7.5 cm (approximately 3 in) in diameter, and they summon a dragon named Shenron. They were created by Kami, and later re-created by Dende. Shenron can grant a wish within its powers as long as it does not create love, repeat a wish he previously granted, surpasses its creator's power, and meets a few more restrictions. Legitimate wishes include eternal youth, immortality, bringing back a scorched forest or even an annihilated planet.",
-    "erpId":1548'''+ str(i+270) + ''',
+    "erpId":2605'''+ str(i+idPlus) + ''',
     "estoque": {
         "quantidade": 28,
         "quantidadeMinima": 11
@@ -33,12 +56,18 @@ for i in range(30):
     },
     "inativo": true,
     "ncm": "51151",
-    "nome": "Kairo Lote '''+ str(i+270) +'''",
+    "nome": "Kairo Lote '''+ str(i+idPlus) +'''",
     "peso": 10,
     "referencia": "Celular",
     "tipoProduto": "Celular",
     "valorPreco": 60,
     "visibilidade": 2
     },''')
+
+    # Escreve
     arquivo.writelines(texto)
+
+    #Fecha o arquivo
     arquivo.close()
+
+print("PROCESSADO!")
