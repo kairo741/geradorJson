82937f7e0e8a44468416ca80536252ccf8beca06
Kairo (Pessoal) <kairo.amorim2001@gmail.com>
2021-08-16 23:17:22 -0300

    Novas constantes, algumas serão removidas pois implementarei um enum no lugar

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,8 +1,13 @@
-
 class Constants {
   static const String MODEL = "model";
 
-
   static const String ACTIVE = "A";
   static const String INACTIVE = "I";
+
+  static const String TYPE_OBJECT = "Object";
+  static const String TYPE_BOOLEAN = "Boolean";
+  static const String TYPE_NUMBER = "Number";
+  static const String TYPE_STRING = "String";
+  static const String TYPE_NULL = "Null";
+  static const String TYPE_ARRAY = "Array";
 }
