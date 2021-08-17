623a7d826f846e7a624695807b445bd1296579a8
Kairo (Pessoal) <kairo.amorim2001@gmail.com>
2021-08-16 23:18:13 -0300

    Service (ainda sem função) e controller para a Home

---
+++ lib/core/controller/home/home-controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:gerador_json/core/controller/home/home-service.dart';
+import 'package:gerador_json/core/model/json-fields.dart';
+import 'package:gerador_json/ui/screens/fill/view/fill-json-page.dart';
+
+class HomeController {
+  HomeService _service = HomeService();
+
+  goToFill(BuildContext context, List<JsonFields> jsons) {
+    FillJsonPage screen = FillJsonPage(jsons: jsons);
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return screen;
+    }));
+  }
+}
---
+++ lib/core/controller/home/home-service.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:gerador_json/core/model/json-fields.dart';
+
+class HomeService {
+  Widget? buildForm(List<JsonFields> _values) {
+  }
+
+}
