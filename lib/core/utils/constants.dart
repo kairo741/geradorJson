9ee2665aa5ea086eba38912f56e204c729cca6e2
Kairo (Pessoal) <kairo.amorim2001@gmail.com>
2021-09-21 01:23:18 -0300

    Novas constantes para automatizar testes

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,5 @@
+import 'package:gerador_json/core/model/json-fields.dart';
+
 class Constants {
   static const String MODEL = "model";
 
@@ -10,4 +12,94 @@ class Constants {
   static const String TYPE_STRING = "String";
   static const String TYPE_NULL = "Null";
   static const String TYPE_ARRAY = "Array";
+
+  static final List<JsonFields> JSON_FIELDS_LIST = [
+    JsonFields(
+        id: 0,
+        name: "MAIN JSON",
+        childObjectStatus: "A",
+        type: Constants.TYPE_BOOLEAN,
+        mainJson: true),
+    JsonFields(
+        id: 0,
+        name: "BOOLEAN TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_BOOLEAN),
+    JsonFields(
+        id: 0,
+        name: "NUMBER TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_NUMBER),
+    JsonFields(
+        id: 0,
+        name: "STRING TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_STRING),
+    JsonFields(
+        id: 0,
+        name: "ARRAY TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_ARRAY),
+    JsonFields(
+        id: 0,
+        name: "OBJECT TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_OBJECT),
+    JsonFields(
+        id: 0,
+        name: "OBJECT TYPE2",
+        childObjectStatus: "A",
+        fatherObjectName: "OBJECT TYPE",
+        type: Constants.TYPE_OBJECT),
+    JsonFields(
+        id: 0,
+        name: "NULL TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "OBJECT TYPE2",
+        type: Constants.TYPE_NULL),
+  ];
+
+  static final List<JsonFields> JSON_FIELDS_TEST_LIST = [
+    JsonFields(
+        id: 0,
+        name: "MAIN JSON",
+        childObjectStatus: "A",
+        type: Constants.TYPE_BOOLEAN,
+        mainJson: true),
+    JsonFields(
+        id: 0,
+        name: "BOOLEAN TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_BOOLEAN),
+    JsonFields(
+        id: 0,
+        name: "OBJECT TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_OBJECT),
+    JsonFields(
+        id: 0,
+        name: "OBJECT TYPE2",
+        childObjectStatus: "A",
+        fatherObjectName: "OBJECT TYPE",
+        type: Constants.TYPE_OBJECT),
+    JsonFields(
+        id: 0,
+        name: "NULL TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "OBJECT TYPE2",
+        type: Constants.TYPE_NULL),
+    JsonFields(
+        id: 0,
+        name: "NUMBER TYPE",
+        childObjectStatus: "A",
+        fatherObjectName: "MAIN JSON",
+        type: Constants.TYPE_NUMBER),
+  ];
 }
