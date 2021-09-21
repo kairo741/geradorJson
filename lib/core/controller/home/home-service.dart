fc407d4f449e7cb06b8698979eacb39a61fc933e
Kairo (Pessoal) <kairo.amorim2001@gmail.com>
2021-09-21 04:05:19 -0300

    Lógica de organização dos JSONs em lita alterada

--- lib/core/controller/home/home-service.dart
+++ lib/core/controller/home/home-service.dart
@@ -1,29 +1,37 @@
-
 import 'package:gerador_json/core/model/json-fields.dart';
 
 class HomeService {
   buildForm(List<JsonFields> jsons) {
     JsonFields fatherObject =
         jsons[jsons.indexWhere((element) => element.mainJson == true)];
+    int fatherObjectIndex =
+        jsons.indexWhere((element) => element.mainJson == true);
+    List<JsonFields> sortedJsons = [];
 
     ///Sort
     for (int i = 0; i < jsons.length - 1; i++) {
-      for (int j = 0; j < jsons.length - i - 1; j++) {
-        if (jsons[j].mainJson == null) {
-          if (jsons[j].fatherObjectName != jsons[j + 1].fatherObjectName) {
-            if (jsons[j].fatherObjectName == fatherObject.name) {
-              var temp = jsons[j + 1];
-              jsons[j + 1] = jsons[j];
-              jsons[j] = temp;
-            } else if (jsons[j + 1].fatherObjectName != jsons[j].name) {
-              var temp = jsons[j + 1];
-              jsons[j + 1] = jsons[j];
-              jsons[j] = temp;
+      for (int j = 0; j < jsons.length - i; j++) {
+        if (jsons[j].mainJson == null || !jsons[j].mainJson!) {
+          if (jsons[j].fatherObjectName == fatherObject.name) {
+            if (sortedJsons.length > 0) {
+              if (!sortedJsons.contains(jsons[j])) {
+                sortedJsons.insert(fatherObjectIndex + 1, jsons[j]);
+              }
+            } else {
+              sortedJsons.insert(0, jsons[j]);
             }
+          } else {
+            if (!sortedJsons.contains(jsons[j])) {
+              var fatherIndex = sortedJsons.indexWhere(
+                      (element) => (element.name == jsons[j].fatherObjectName));
+              sortedJsons.insert(fatherIndex + 1, jsons[j]);
+            }
+
           }
         }
       }
     }
-    print(jsons);
+    // index - 1 da lista inicial, pois, não contém o objeto pai
+    print(sortedJsons);
   }
 }
